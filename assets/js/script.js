9475464dfd73033e27fda97166f56eb6f2fc744a
Shahnilsharma <shahnil.sharma402@gmail.com>
2025-08-02 01:19:47 +0500

    integrated chat bot

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,7 +199,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/68814b99debf57191758474b/1j0sguk7e';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
